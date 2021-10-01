a47c66405d04fff6589a2ff3b0f68c988a83d6f2
Sawarni Swaroop <sawarni99@gmail.com>
2021-10-02 01:05:59 +0530

    Added basic functions of database.

--- db_handler.py
+++ db_handler.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from constants import MAX_SIZE
 
 # Create env.py file to set connection, db and collection name from mongoDB
 from env import CONNECTION_LINK, DB_NAME, COLLECTION_NAME
@@ -17,3 +18,35 @@ class Database:
         self.collection = db[COLLECTION_NAME]
 
 
+    # Getting count of datas in db...
+    def count(self):
+        return self.collection.count({})
+
+
+    # initializing DB...
+    def initialize_db(self):
+
+        # Checking if DB is empty...
+        if self.count() == 0:
+
+            # Interating through max size...
+            for index in range(MAX_SIZE):
+
+                # Initializing DB with max size ids...
+                self.collection.insert_one({"_id" : index+1})
+
+
+    # Function to update datas...
+    def update(self, texts):
+
+        # Iterating through the texts...
+        for index in range(texts):
+
+            # Updating the new values...
+            self.collection.update({"_id": index+1}, {"$set":{"text" : texts[index]}})
+
+    
+    # Function to get all datas...
+    def get_data():
+        # TODO :: Have to code here....
+        pass
